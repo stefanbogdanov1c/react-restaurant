49fea69c673636b9b61a2a7c37c37c3ee3a8e0e8
stefanbogdanov1c <stefanbogdanovic2505@gmail.com>
2023-02-12 19:10:56 +0100

    feat/landing page

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-
+import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
-  </div>
+  <div className="app__aboutus app__bg flex__center section__pading" id='about'></div>
 );
 
 export default AboutUs;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { SubHeading } from '../../components';
 
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id='home'>
+    <div className="app__wrapper_info">
+    <SubHeading title="Chase the new flavour"/>
+      <h1 className='app__header-h1'>The key to fine dining</h1>
+      <p className="p__opensans" style={{margin:'2rem 0'}}>Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem ipsam odit</p>
+      <button className='custom__button'>Explore menu</button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="welcome!" />
+    </div>
   </div>
 );
 
