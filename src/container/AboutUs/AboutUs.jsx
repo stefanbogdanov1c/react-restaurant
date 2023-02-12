ad40a632b4166a986d0eaa69b1257985c08c7d3c
stefanbogdanov1c <stefanbogdanovic2505@gmail.com>
2023-02-12 19:58:52 +0100

    feat/about us

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -3,7 +3,41 @@ import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div className="app__aboutus app__bg flex__center section__pading" id='about'></div>
+  <div className="app__aboutus app__bg flex__center section__pading" id='about'>
+
+    <div className="app__aboutus-overlay flex__center">
+
+      <img src={images.G} alt="G" />
+
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+
+      <div className="app__aboutus-content_about">
+
+        <h1 className="headtext__cormorant">About us</h1>
+        <img src={images.spoon} alt="spoon" className='spoon__img'/>
+        <p className="p__opensans">neki tekst</p>
+        <button className="custom__button">Know more</button>
+
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="noz" />
+      </div>
+      
+      <div className="app__aboutus-content_history">
+
+        <h1 className="headtext__cormorant">Our history</h1>
+        <img src={images.spoon} alt="spoon" className='spoon__img'/>
+        <p className="p__opensans">neki tekst</p>
+        <button className="custom__button">Know more</button>
+
+      </div>
+
+    </div>
+
+  </div>
 );
 
 export default AboutUs;
